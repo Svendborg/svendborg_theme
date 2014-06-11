6c44f5cef90950a546dc4127564dfa74852619d6
Anders Bryrup <andersbryrup@gmail.com>
2014-06-11 10:54:05 +0200

    Add some default examples of how to use preprocessors.

--- template.php
+++ template.php
@@ -3,3 +3,45 @@
  * @file
  * template.php
  */
+
+
+/**
+ * Implements hook_preprocess_node().
+ */
+function svendborg_theme_preprocess_node(&$variables) {
+  /*
+   * If page shouldn't have a right sidebar, this is check by a checkbox on
+   * node/term. Remove it from the render array.
+   */
+/**
+ * THIS IS ONLY AN EXAMPLE!!!
+  $node = $variables['node'];
+
+  // Make the field variables available with the appropriate language.
+  field_attach_preprocess('node', $node, $variables['content'], $variables);
+
+  if (!$variables['has_right_column_field']) {
+    $variables['classes_array'][] = 'without-right-sidebar';
+  }
+*/
+}
+
+
+/**
+ * Implements hook_preprocess_region().
+ */
+function svendborg_theme_preprocess_region(&$variables) {
+  if (isset($variables['page']['node']) && !empty($variables['page']['node']->nid)) {
+    $node = $variables['page']['node'];
+  }
+
+  // Region Sidebar Second.
+  if ($variables['region'] === 'sidebar_second') {
+
+    // Show related links in sidebar checkbox on node.
+    if ($node && $node->field_os2web_base_field_hidlinks['und'][0]['value'] == '0') {
+      // Show related links in reqion. Add them to the render array somehow.
+      // $variables['elements']['related_links'] = render(elements);
+    }
+  }
+}
