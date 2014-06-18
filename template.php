fb939442ab66cbd21c3b48ca189edfb72f9c1fa5
Anders Bryrup <andersbryrup@gmail.com>
2014-06-18 16:14:34 +0200

    Added styles to sidebar_first, breadcrumb and menus.

--- template.php
+++ template.php
@@ -63,12 +63,51 @@ function svendborg_theme_preprocess_page($variables) {
   ), 'google_font_svendborg_theme');
 }
 
+/**
+ * Implements THEME_preprocess_html().
+ */
 function svendborg_theme_preprocess_html(&$variables) {
-  // Add conditional stylesheets for IE
+  // Add conditional stylesheets for IE.
   drupal_add_css(path_to_theme() . '/css/ie.css', array(
     'group' => CSS_THEME,
     'browsers' => array('IE' => 'lte IE 8', '!IE' => FALSE),
     'preprocess' => FALSE,
-    'weight' => 115
+    'weight' => 115,
   ));
+}
+
+/**
+ * Implements theme_breadcrumb().
+ *
+ * Output breadcrumb as an unorderd list with unique and first/last classes.
+ */
+function svendborg_theme_breadcrumb($variables) {
+  $breadcrumbs = $variables['breadcrumb'];
+  if (!empty($breadcrumbs)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $crumbs = '<ul class="breadcrumb">';
+
+    foreach ($breadcrumbs as $breadcrumb) {
+      $classes = array();
+      if ($breadcrumb == reset($breadcrumbs)) {
+        $classes[] = 'first';
+      }
+      if ($breadcrumb == end($breadcrumbs)) {
+        $classes[] = 'last';
+      }
+      if (is_array($breadcrumb)) {
+        if (isset($breadcrumb['class'])) {
+          $classes = array_merge($classes, $breadcrumb['class']);
+        }
+        if (isset($breadcrumb['data'])) {
+          $breadcrumb = $breadcrumb['data'];
+        }
+      }
+      $crumbs .= '<li class="' . implode(' ', $classes) . '"><i></i>'  . $breadcrumb . '</li>';
+    }
+    $crumbs .= '</ul>';
+    return $crumbs;
+  }
 }
--- theme/node.tpl.php
+++ theme/node.tpl.php
@@ -82,6 +82,14 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if ((!$page && !empty($title)) || !empty($title_prefix) || !empty($title_suffix) || $display_submitted): ?>
   <header>
+    <?php if ($type == 'os2web_base_news') : ?>
+      <?php print render($content['field_os2web_base_field_image']); ?>
+    <?php else : ?>
+      <?php print render($content['field_os2web_base_field_lead_img']); ?>
+    <?php endif; ?>
+    <time pubdate="pubdate">
+      <?php print $date; ?>
+    </time>
     <?php print render($title_prefix); ?>
     <?php if (!$page && !empty($title)): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
@@ -100,6 +108,7 @@
     hide($content['comments']);
     hide($content['links']);
     hide($content['field_tags']);
+    hide($content['field_os2web_base_field_image']);
     print render($content);
   ?>
   <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
