3ed0004afc7053c21a1a59d32ecb5ab6a72bea2f
Anders Bryrup <andersbryrup@gmail.com>
2014-06-19 10:36:45 +0200

    Hide sidebar_second if the field on node is checked.

--- template.php
+++ template.php
@@ -4,53 +4,22 @@
  * template.php
  */
 
-
-/**
- * Implements hook_preprocess_node().
- */
-function svendborg_theme_preprocess_node(&$variables) {
-  /*
-   * If page shouldn't have a right sidebar, this is check by a checkbox on
-   * node/term. Remove it from the render array.
-   */
-/**
- * THIS IS ONLY AN EXAMPLE!!!
-  $node = $variables['node'];
-
-  // Make the field variables available with the appropriate language.
-  field_attach_preprocess('node', $node, $variables['content'], $variables);
-
-  if (!$variables['has_right_column_field']) {
-    $variables['classes_array'][] = 'without-right-sidebar';
-  }
-*/
-}
-
-
-/**
- * Implements hook_preprocess_region().
- */
-function svendborg_theme_preprocess_region(&$variables) {
-  $node = NULL;
-  if (isset($variables['page']['node']) && !empty($variables['page']['node']->nid)) {
-    $node = $variables['page']['node'];
-  }
-
-  // Region Sidebar Second.
-  if ($variables['region'] === 'sidebar_second') {
-
-    // Show related links in sidebar checkbox on node.
-    if ($node && $node->field_os2web_base_field_hidlinks['und'][0]['value'] == '0') {
-      // Show related links in reqion. Add them to the render array somehow.
-      // $variables['elements']['related_links'] = render(elements);
-    }
-  }
-}
-
 /**
  * Implements template_preprocess_page().
  */
-function svendborg_theme_preprocess_page($variables) {
+function svendborg_theme_preprocess_page(&$variables) {
+  $node = NULL;
+  if (isset($variables['node']) && !empty($variables['node']->nid)) {
+    $node = $variables['node'];
+  }
+
+  // If node has hidden the sidebar, set content to null and return.
+  if ($node &&
+      isset($node->field_svendborg_hide_sidebar['und'][0]['value']) &&
+      $node->field_svendborg_hide_sidebar['und'][0]['value'] == '1') {
+
+    $variables['page']['sidebar_second'] = array();
+  }
 
   // Add out fonts from Google Fonts API.
   drupal_add_html_head(array(
