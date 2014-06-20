21083a59c2e3d4ec8424e211c0fad346198c1c51
Anders Bryrup <andersbryrup@gmail.com>
2014-06-20 17:32:39 +0200

    Some minor mods to font-size and columns.

--- template.php
+++ template.php
@@ -123,7 +123,7 @@ function svendborg_theme_preprocess_page(&$variables) {
     $spotbox_array = os2web_spotbox_render_spotboxes($spotbox_nids, NULL, NULL, NULL, 'svendborg_spotbox');
 
     foreach ($spotbox_array['node'] as &$spotbox) {
-      $spotbox['#prefix'] = '<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">';
+      $spotbox['#prefix'] = '<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">';
       $spotbox['#suffix'] = '</div>';
     }
 
